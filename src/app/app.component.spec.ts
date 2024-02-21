c55557699b378417a22145cc3e2677c275d24a80
Will <doggy.huang@gmail.com>
2024-02-21 18:08:10 +0800

    加入 HttpClientTestingModule 測試範例

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { firstValueFrom } from 'rxjs';
+
+let httpTestingController: HttpTestingController;
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create the app', () => {
@@ -26,10 +30,34 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('demo2');
   });
 
+  it(`should req'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const mockData = { data: 'test' };
+
+    console.log('1');
+    app.getData().subscribe(data => {
+      expect(data).toEqual(mockData.data);
+    });
+
+    var req = httpTestingController.expectOne('http://localhost:4200/assets/a.json', 'Home page');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockData);
+  });
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('demo2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'demo2 app is running!'
+    );
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo2';
+
+  constructor(private http: HttpClient) { }
+
+  getData() {
+    return this.http.get<any>('http://localhost:4200/assets/a.json')
+      .pipe(
+        map((data) => data.data)
+      );
+  }
 }
